c38ff0a358581e520d334be1fca06df5ce0ec100
tomasgueri <tomas_053@hotmail.es>
2021-07-09 22:23:01 -0300

    campos dinamicos

--- src/pages/Retiro.jsx
+++ src/pages/Retiro.jsx
@@ -4,7 +4,8 @@ import "../assets/retiro.css";
 
 export default function Inversion() {
   const [tipo, setTipo] = useState("usdt");
-  const [acumulado, setAcumulado] = useState(23000);
+  const [disponible, setDisponible] = useState(3200);
+  const [importe, setImporte] = useState(2800);
   const [billetera, setBilletera] = useState('1JJH231JKH2312HJ3H3K21J33BBJKJK')
 
   return (
@@ -13,9 +14,9 @@ export default function Inversion() {
         <div className="pt-5">
           <div className="col-12 col-lg-6 m-auto dinero-disponible px-3">
             <h2>Dinero disponible</h2>
-            <h3 className="my-4">USD {acumulado}</h3>
-            <h2>Dinero disponible</h2>
-            <h3 className="mt-4">USD {acumulado}</h3>
+            <h3 className="my-4">USD {disponible}</h3>
+            <h2>Importe de retiro</h2>
+            <h3 className="mt-4">USD {importe}</h3>
           </div>
           <div className="col-12 mt-4">
                 <div className="col-12 col-md-7 py-1 m-auto billetera-retiro">
