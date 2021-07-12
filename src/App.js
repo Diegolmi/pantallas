b0ebe1dd540a9df3c99d7f82c4d38420bd5014d8
tomasgueri <tomas_053@hotmail.es>
2021-07-12 20:13:24 -0300

    blog fondo rosa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Inversion from './pages/Inversion'
-import Retiro from './pages/Retiro'
+import Inversion from "./pages/Inversion";
+import Retiro from "./pages/Retiro";
 import Soporte from "./pages/Soporte";
 import Objetivos from "./pages/Objetivos";
 import Perfil from "./pages/Perfil";
 import Blog from "./pages/Blog";
 import NavReactB from "./components/NavReactB";
 import SideBar from "./components/SideBar";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 
 function App() {
+  let location = useLocation();
+  const [path, setPath] = useState(true);
+
+  useEffect(() => {
+    if (location.pathname === '/Blog') {
+      setPath(false)
+    }
+  }, [])
+
+
   return (
     <div className="App">
       <NavReactB />
-      <div className="d-flex" style={{minHeight: '100vh'}}>
-        <SideBar />
+      <div className="d-flex" style={{ minHeight: "100vh" }}>
+        {path === true && <SideBar />}
         <div className="w-100">
           <Switch>
-            <Route path="/" exact>
-            </Route>
+            <Route path="/" exact></Route>
             <Route path="/Inversion" exact>
               <Inversion />
             </Route>
